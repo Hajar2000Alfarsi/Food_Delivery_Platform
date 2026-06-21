f259f04f9609386eb7a880248ca18c8c0190d6bc
Hajar <hajar2000alfarsi@gmail.com>
2026-06-21 11:44:25 +0400

    Create MenuItem class with required fields

--- src/main/java/com/example/Food/Delivery/Platform/Entities/MenuItem.java
+++ src/main/java/com/example/Food/Delivery/Platform/Entities/MenuItem.java
@@ -1,9 +1,34 @@
 package com.example.Food.Delivery.Platform.Entities;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Entity
-@
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class MenuItem extends BaseClass {
+    private String name;
+    private String description;
+    private Double price;
 
+    private Boolean isAvailable;
+    private Boolean isVegetarian;
+
+    private Integer calories;
+
+    @ManyToOne
+    private Restaurant restaurant;
+
+    @OneToMany(mappedBy = "menuItems")
+    private List<ComboMeal> comboMeals;
+
+    @OneToMany(mappedBy = "menuItems")
+    private  List<OrderItem> orderItems;
 }
