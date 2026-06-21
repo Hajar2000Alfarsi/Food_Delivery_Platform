188e9ce1ce84c2373b943765acae330095d5e953
Hajar <hajar2000alfarsi@gmail.com>
2026-06-21 11:28:55 +0400

    Create RestaurantOwner class with required fields

---
+++ src/main/java/com/example/Food/Delivery/Platform/Entities/RestaurantOwner.java
@@ -0,0 +1,20 @@
+package com.example.Food.Delivery.Platform.Entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@AllArgsConstructor
+@Data
+@NoArgsConstructor
+public class RestaurantOwner extends BaseUser{
+    private String businessLicenseCode;
+
+    @OneToMany(mappedBy = "owner")
+    private List<Restaurant> restaurants;
+}
