33cc6427dc67107eb0abb788fa433236f9bd6c07
Hajar <hajar2000alfarsi@gmail.com>
2026-06-21 16:13:20 +0400

    Create ComboMealRepository whith queries

---
+++ src/main/java/com/example/Food/Delivery/Platform/Repositories/ComboMealRepository.java
@@ -0,0 +1,14 @@
+package com.example.Food.Delivery.Platform.Repositories;
+
+import com.example.Food.Delivery.Platform.Entities.ComboMeal;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface ComboMealRepository extends JpaRepository<ComboMeal, Integer> {
+    @Query("select c from ComboMeal c join c.menuItem m where m.id = :menuItemId AND c.isActive = true")
+    List<ComboMeal> findByMenuItemId(@Param("menuItemId") Integer menuItemId);
+
+}
