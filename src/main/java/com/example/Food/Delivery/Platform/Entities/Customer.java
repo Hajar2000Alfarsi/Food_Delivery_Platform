ae7aaedfe7be7bd354c6bbba4b0c4edc73077778
Hajar <hajar2000alfarsi@gmail.com>
2026-06-21 11:22:03 +0400

    Create Customer class with required fields

--- src/main/java/com/example/Food/Delivery/Platform/Entities/Customer.java
+++ src/main/java/com/example/Food/Delivery/Platform/Entities/Customer.java
@@ -1,13 +1,28 @@
 package com.example.Food.Delivery.Platform.Entities;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class Customer extends Base{
+public class Customer extends BaseUser{
+    private Integer loyaltyPoints;
+    private String customerCode;
+
+    @OneToMany(mappedBy = "customer")
+    private  List<CustomerAddress> addresses;
+
+    @OneToMany(mappedBy = "customer")
+    private  List<Order> orders;
+
+    @OneToMany(mappedBy = "customer")
+    private List<Review> reviews;
+
 }
