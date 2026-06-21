b363bb543fb54ea3f94403228cf898543ea2fe32
Hajar <hajar2000alfarsi@gmail.com>
2026-06-21 12:18:02 +0400

    Create Payment class with required fields

---
+++ src/main/java/com/example/Food/Delivery/Platform/Entities/Payment.java
@@ -0,0 +1,26 @@
+package com.example.Food.Delivery.Platform.Entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Payment extends BaseClass{
+    private String paymentMethod;
+    private String status;
+    private String transactionRef;
+
+    private Double amount;
+
+    private LocalDateTime processedAt;
+
+    @OneToOne
+    private Order order;
+}
