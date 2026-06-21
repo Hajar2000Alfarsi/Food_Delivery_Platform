6c96b7b5144872a9ea956db964685f5a5ca366bd
Hajar <hajar2000alfarsi@gmail.com>
2026-06-21 11:01:21 +0400

    Create base class which will be parent class with nedded fields

---
+++ src/main/java/com/example/Food/Delivery/Platform/Entities/Base.java
@@ -0,0 +1,30 @@
+package com.example.Food.Delivery.Platform.Entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@Data
+public class Base {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Integer id;
+
+    private LocalDateTime createDate;
+    private LocalDateTime updateDate;
+    private Boolean isActive;
+
+    @PrePersist
+    public void prePersist(){
+        createDate = LocalDateTime.now();
+        isActive = true;
+    }
+
+    @PreUpdate
+    public void preUpdate(){
+        updateDate = LocalDateTime.now();
+    }
+}
