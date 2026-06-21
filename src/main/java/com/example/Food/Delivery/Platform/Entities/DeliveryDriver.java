046048d5dfee0a6800a0adfcaaf335987294eb57
Hajar <hajar2000alfarsi@gmail.com>
2026-06-21 11:26:39 +0400

    Create DeliveryDriver class with required fields

---
+++ src/main/java/com/example/Food/Delivery/Platform/Entities/DeliveryDriver.java
@@ -0,0 +1,27 @@
+package com.example.Food.Delivery.Platform.Entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class DeliveryDriver extends BaseUser{
+    private String driverCode;
+    private String vehicleType;
+    private String vehiclePlate;
+
+    private Double currentLat;
+    private Double currentLng;
+
+    private Boolean isOnline;
+
+    @OneToMany(mappedBy = "driver")
+    private List<Delivery> deliveries;
+}
