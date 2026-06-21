81307eefcc9b08f7b0eaaee367a5e96c8d1f19bc
Hajar <hajar2000alfarsi@gmail.com>
2026-06-21 14:58:59 +0400

    Create customerREpository whith queries

---
+++ src/main/java/com/example/Food/Delivery/Platform/Repositories/CustomerRepository.java
@@ -0,0 +1,23 @@
+package com.example.Food.Delivery.Platform.Repositories;
+
+import com.example.Food.Delivery.Platform.Entities.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface CustomerRepository extends JpaRepository {
+    @Query("select c from Customer where c.email=:email AND c.isActive=true")
+    Customer findByEmail(@Param("email") String email);
+
+    @Query("select c from Customer where c.loyaltyPoints >= :points AND c.isActive = true")
+    List<Customer> findByLoyaltyPointsGreaterThanEqual(@Param("points") Integer loyaltyPoints);
+
+    @Query("select c from Customer c where c.createdDate BETWEEN :start AND :end AND c.isActive = true")
+    List<Customer> findCustomersByDateRange(@Param("start") LocalDateTime start,
+                                            @Param("end") LocalDateTime end);
+
+
+}
