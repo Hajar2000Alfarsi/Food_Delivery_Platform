ca6de85dd49b5526e99997d6b74a16ce7fff40b5
Hajar <hajar2000alfarsi@gmail.com>
2026-06-21 15:38:37 +0400

    Create RestaurantRepository whith queries

--- src/main/java/com/example/Food/Delivery/Platform/Repositories/CustomerRepository.java
+++ src/main/java/com/example/Food/Delivery/Platform/Repositories/CustomerRepository.java
@@ -8,11 +8,11 @@ import org.springframework.data.repository.query.Param;
 import java.time.LocalDateTime;
 import java.util.List;
 
-public interface CustomerRepository extends JpaRepository {
-    @Query("select c from Customer where c.email=:email AND c.isActive=true")
+public interface CustomerRepository extends JpaRepository<Customer, Integer> {
+    @Query("select c from Customer c where c.email=:email AND c.isActive=true")
     Customer findByEmail(@Param("email") String email);
 
-    @Query("select c from Customer where c.loyaltyPoints >= :points AND c.isActive = true")
+    @Query("select c from Customer c where c.loyaltyPoints >= :points AND c.isActive = true")
     List<Customer> findByLoyaltyPointsGreaterThanEqual(@Param("points") Integer loyaltyPoints);
 
     @Query("select c from Customer c where c.createdDate BETWEEN :start AND :end AND c.isActive = true")
