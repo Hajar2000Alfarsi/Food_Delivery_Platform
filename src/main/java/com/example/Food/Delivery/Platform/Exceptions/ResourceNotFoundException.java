04f149662b1c43ae6160ceb020941da25716beee
Hajar <hajar2000alfarsi@gmail.com>
2026-06-22 22:27:32 +0400

    Create ResourceNotFoundException class

---
+++ src/main/java/com/example/Food/Delivery/Platform/Exceptions/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.example.Food.Delivery.Platform.Exceptions;
+
+public class ResourceNotFoundException extends RuntimeException{
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
