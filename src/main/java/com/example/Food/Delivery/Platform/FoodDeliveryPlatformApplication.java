59e0520ae81491ab2be540dee8918ceb908be0c0
Hajar <hajar2000alfarsi@gmail.com>
2026-06-22 11:19:26 +0400

    Create RestaurantResponseDTO class with required fields

--- src/main/java/com/example/Food/Delivery/Platform/FoodDeliveryPlatformApplication.java
+++ src/main/java/com/example/Food/Delivery/Platform/FoodDeliveryPlatformApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FoodDeliveryPlatformApplication {
+public class 	FoodDeliveryPlatformApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FoodDeliveryPlatformApplication.class, args);
