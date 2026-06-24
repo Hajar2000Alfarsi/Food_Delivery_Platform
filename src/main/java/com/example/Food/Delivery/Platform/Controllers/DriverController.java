cc69e24128ae861a1615325ec39d17b2da98b555
Hajar <hajar2000alfarsi@gmail.com>
2026-06-24 08:43:55 +0400

    Implement createDriver method in DriverController

--- src/main/java/com/example/Food/Delivery/Platform/Controllers/DriverController.java
+++ src/main/java/com/example/Food/Delivery/Platform/Controllers/DriverController.java
@@ -1,9 +1,34 @@
 package com.example.Food.Delivery.Platform.Controllers;
 
+import com.example.Food.Delivery.Platform.DTO.request.DriverRequestDTO;
+import com.example.Food.Delivery.Platform.DTO.response.DriverResponseDTO;
+import com.example.Food.Delivery.Platform.Services.DriverService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/derivers")
 public class DriverController {
+
+    private final DriverService driverService;
+
+    @Autowired
+
+    public DriverController(DriverService driverService) {
+        this.driverService = driverService;
+    }
+
+    //Register driver
+    @PostMapping
+    public ResponseEntity<DriverResponseDTO> createDriver(@Valid @RequestBody DriverRequestDTO dto) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(driverService.createDriver(dto));
+    }
+
+
 }
