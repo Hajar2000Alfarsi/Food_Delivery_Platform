9e07ec09e559042cf3d3ff8322d356362a987a02
Hajar <hajar2000alfarsi@gmail.com>
2026-06-25 18:19:49 +0400

    Implement payment.setOrder(order);

---
+++ src/main/java/com/example/Food/Delivery/Platform/Services/PaymentService.java
@@ -0,0 +1,56 @@
+package com.example.Food.Delivery.Platform.Services;
+
+import com.example.Food.Delivery.Platform.DTO.response.PaymentResponseDTO;
+import com.example.Food.Delivery.Platform.Entities.FoodOrder;
+import com.example.Food.Delivery.Platform.Entities.Payment;
+import com.example.Food.Delivery.Platform.Exceptions.ResourceNotFoundException;
+import com.example.Food.Delivery.Platform.Repositories.OrderRepository;
+import com.example.Food.Delivery.Platform.Repositories.PaymentRepository;
+import com.example.Food.Delivery.Platform.Utils.HelperUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+public class PaymentService {
+    private final PaymentRepository paymentRepository;
+    private final OrderRepository orderRepository;
+
+    @Autowired
+
+    public PaymentService(PaymentRepository paymentRepository, OrderRepository orderRepository) {
+        this.paymentRepository = paymentRepository;
+        this.orderRepository = orderRepository;
+    }
+
+    //process payment
+    public PaymentResponseDTO processPayment(Integer orderId, String method) {
+        FoodOrder order = orderRepository.findByActiveId(orderId)
+                .orElseThrow(() ->
+                        new ResourceNotFoundException("Order not found"));
+
+        Payment payment = new Payment();
+
+        payment.setPaymentMethod(method);
+        payment.setStatus("PENDING");
+        payment.setTransactionRef(HelperUtils.generateCode("PAY"));
+
+        payment.setAmount(order.getTotalAmount());
+        payment.setOrder(order);
+
+        payment.setCreatedDate(LocalDateTime.now());
+        payment.setIsActive(true);
+
+        return PaymentResponseDTO.fromEntity(paymentRepository.save(payment));
+    }
+
+
+    //find payment
+    public Payment findPayment(Integer id) {
+        return paymentRepository.findByActiveId(id)
+                .orElseThrow(() ->
+                        new ResourceNotFoundException("Payment not found"));
+    }
+
+}
