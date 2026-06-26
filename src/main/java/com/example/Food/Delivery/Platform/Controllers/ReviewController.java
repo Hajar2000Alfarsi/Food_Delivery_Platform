7bf994f838dcfb95ef1d3a8f4820cb03b10ff330
Hajar <hajar2000alfarsi@gmail.com>
2026-06-26 10:56:47 +0400

    Create Review Controller with restaurant review method

---
+++ src/main/java/com/example/Food/Delivery/Platform/Controllers/ReviewController.java
@@ -0,0 +1,34 @@
+package com.example.Food.Delivery.Platform.Controllers;
+
+import com.example.Food.Delivery.Platform.DTO.request.ReviewRequestDTO;
+import com.example.Food.Delivery.Platform.DTO.response.ReviewResponseDTO;
+import com.example.Food.Delivery.Platform.Services.ReviewService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/reviews")
+public class ReviewController {
+    private final ReviewService reviewService;
+
+    @Autowired
+
+    public ReviewController(ReviewService reviewService) {
+        this.reviewService = reviewService;
+    }
+
+    //RESTAURANT REVIEW
+    @PostMapping("/restaurant/{restaurantId}/customer/{customerId}")
+    public ResponseEntity<ReviewResponseDTO> leaveRestaurantReview(
+            @PathVariable Integer restaurantId,
+            @PathVariable Integer customerId,
+            @RequestBody ReviewRequestDTO dto) {
+
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(reviewService.leaveRestaurantReview(customerId, restaurantId, dto.getRating(), dto.getComment()));
+    }
+
+
+}
