2a0d49b23f5475ffb646759a77b082841b6400f1
Hajar <hajar2000alfarsi@gmail.com>
2026-06-26 15:27:53 +0400

    Create DriverLeaderboardDTO

--- src/main/java/com/example/Food/Delivery/Platform/DTO/response/report/DriverLeaderboardDTO.java
+++ src/main/java/com/example/Food/Delivery/Platform/DTO/response/report/DriverLeaderboardDTO.java
@@ -1,4 +1,14 @@
 package com.example.Food.Delivery.Platform.DTO.response.report;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class DriverLeaderboardDTO {
+    private Integer driverId;
+    private String driverName;
+    private Long completedDeliveries;
 }
