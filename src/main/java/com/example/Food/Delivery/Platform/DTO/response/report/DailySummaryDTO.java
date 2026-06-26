b81538eda35a17ce4b9a56432a06fb189677bd17
Hajar <hajar2000alfarsi@gmail.com>
2026-06-26 15:26:51 +0400

    Create DailySummeryDTO

---
+++ src/main/java/com/example/Food/Delivery/Platform/DTO/response/report/DailySummaryDTO.java
@@ -0,0 +1,14 @@
+package com.example.Food.Delivery.Platform.DTO.response.report;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class DailySummaryDTO {
+    private String date;
+    private Long totalOrders;
+    private Double totalDeliveryFees;
+}
